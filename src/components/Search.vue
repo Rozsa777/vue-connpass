7c887eeb7c24f661c5d200056a45400124cb3b4a
Hideki Tsuchiya <htsuchiya@htsuchiya.local>
2019-05-05 14:25:27 +0900

    first commit

---
+++ src/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="search-conditoins">
+    <dl class="conditoin">
+      <dt class="conditoin-name">都道府県</dt>
+      <dd class="conditoin-item">
+        <input type="text" placeholder="都道府県">
+      </dd>
+    </dl>
+    <dl class="conditoin">
+      <dt class="conditoin-name">開催日</dt>
+      <dd class="conditoin-item">
+        <input type="text" placeholder="From">
+      </dd>
+      <dd class="conditoin-item">
+        <input type="text" placeholder="To">
+      </dd>
+    </dl>
+    <dl class="conditoin">
+      <dt class="conditoin-name">キーワード</dt>
+      <dd class="conditoin-item">
+        <input type="text" placeholder="キーワード">
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search'
+}
+</script>
